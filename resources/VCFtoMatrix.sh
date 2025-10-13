47609b38c34ea5d5a17caf5905c3f5cbd2778d34
rmdp <r.d.pasco@cgiar.org>
2025-10-13 14:15:19 +0800

    Update VCFtoMatrix.sh

--- resources/VCFtoMatrix.sh
+++ resources/VCFtoMatrix.sh
@@ -13,8 +13,6 @@ bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%TGT]\n' $invcf |  tr "|" "/"  | \
 
 cut -f1-4 $pref/mat_vcf.txt > $pref/pos.txt
 
-# The following fails for some reason (grep CHR part)
-
 bcftools view -o -   -h $invcf  |  grep CHR | tr "\t" "\n"  | tail -n+10 > $pref/sample_list.txt
 
 
